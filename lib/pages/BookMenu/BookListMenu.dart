f4672c09d4cf26d35560eb80113efec957664cb6
Dagmawi Abebe <52936941+dagiabea@users.noreply.github.com>
2024-01-02 21:49:34 +0300

    store to local added

--- lib/pages/BookMenu/BookListMenu.dart
+++ lib/pages/BookMenu/BookListMenu.dart
@@ -144,18 +144,18 @@ class _BookListPageState extends State<BookListPage> {
     }) as StreamSubscription<List<PurchaseDetails>>;
 
     //!Check Subscription Availability
-    // IAPService(
-    //         monthlyProductId: Platform.isAndroid
-    //             ? widget.configResponse.androidSettings.subscriptionSettings
-    //                 .monthSubscriptionProductID!
-    //             : widget.configResponse.iosSettings.subscriptionSettings
-    //                 .monthSubscriptionProductID!,
-    //         yearlyProductId: Platform.isAndroid
-    //             ? widget.configResponse.androidSettings.subscriptionSettings
-    //                 .yearSubscriptionProductID!
-    //             : widget.configResponse.iosSettings.subscriptionSettings
-    //                 .yearSubscriptionProductID!)
-    //     .checkSubscriptionAvailabilty();
+    IAPService(
+            monthlyProductId: Platform.isAndroid
+                ? widget.configResponse.androidSettings.subscriptionSettings
+                    .monthSubscriptionProductID!
+                : widget.configResponse.iosSettings.subscriptionSettings
+                    .monthSubscriptionProductID!,
+            yearlyProductId: Platform.isAndroid
+                ? widget.configResponse.androidSettings.subscriptionSettings
+                    .yearSubscriptionProductID!
+                : widget.configResponse.iosSettings.subscriptionSettings
+                    .yearSubscriptionProductID!)
+        .checkSubscriptionAvailabilty();
 
     // loadSubscriptionStatus();
     //checkSubscriptionValidity();
